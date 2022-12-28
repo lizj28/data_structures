7d4f4c680381fdc5f30736364dae7f65d30bf6e3
Liz Johnson <lizjohnson@Lizs-MacBook-Pro.local>
2022-12-28 14:30:01 -0500

    insert and find for binary tree

--- app/binary_tree.py
+++ app/binary_tree.py
@@ -1,13 +1,37 @@
 class TreeNode:
-    def __init__(self):
+    def __init__(self, data: int):
         self.right_child = None
         self.left_child = None
-        self.data = None
+        self.data = data
 
-# Insertion
 # Removal
 # Search
+# Height of tree
 class BinaryTree:
     def __init__(self):
         self.root = None
-        
+
+    def is_empty(self):
+        if self.root is None:
+            return True
+        return False
+
+    def insert(self, root_node: TreeNode, node: TreeNode):
+        if root_node is None:
+            root_node = node
+            return root_node
+        if root_node.data <= node.data:
+            root_node.right_child = self.insert(root_node.right_child, node)
+        elif root_node.data > node.data:
+            root_node.left_child = self.insert(root_node.left_child, node)
+        return root_node
+
+    def find(self, root_node: TreeNode, val):
+        if root_node is None:
+            return None
+        if val == root_node.data:
+            return root_node
+        if val > root_node.data:
+            return self.find(root_node.right_child, val)
+        elif val < root_node.data:
+            return self.find(root_node.left_child, val)
---
+++ app/test_binary_tree.py
@@ -0,0 +1,46 @@
+import unittest
+
+from app.binary_tree import BinaryTree, TreeNode
+
+
+class TestBinaryTree(unittest.TestCase):
+    def setUp(self):
+        self.tree: BinaryTree = BinaryTree()
+        self.tree_node: TreeNode = TreeNode(5)
+
+    def test_is_empty(self):
+        assert self.tree.is_empty() is True
+
+        self.tree.root = self.tree_node
+        assert self.tree.is_empty() is False
+
+    def test_insert(self):
+        self.tree.root = self.tree_node
+        right_node: TreeNode = TreeNode(6)
+        left_node: TreeNode = TreeNode(4)
+        self.tree.insert(self.tree.root, right_node)
+        self.tree.insert(self.tree.root, left_node)
+
+        assert self.tree.root.right_child.data == 6
+        assert self.tree.root.left_child.data == 4
+
+    def test_remove(self):
+        # TODO
+        assert True is False
+
+    def test_find(self):
+        self.tree.root = self.tree_node
+        right_node: TreeNode = TreeNode(6)
+        left_node: TreeNode = TreeNode(4)
+        right_right_node: TreeNode = TreeNode(8)
+        self.tree.insert(self.tree.root, right_node)
+        self.tree.insert(self.tree.root, left_node)
+        self.tree.insert(self.tree.root, right_right_node)
+
+        node_found: TreeNode = self.tree.find(self.tree.root, 8)
+        node_not_found: TreeNode = self.tree.find(self.tree.root, 10)
+
+        assert node_found == right_right_node
+        assert node_not_found is None
+
+
